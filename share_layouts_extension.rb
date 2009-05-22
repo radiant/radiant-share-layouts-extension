51cd02fcca13a0a6a4fe1964f6ad09aa0fce83f8
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2009-05-22 12:23:45 +0200

    Changed application to application_controller to make it 0.8RC1 compatible

--- share_layouts_extension.rb
+++ share_layouts_extension.rb
@@ -1,8 +1,8 @@
 # Uncomment this if you reference any of your controllers in activate
-require_dependency 'application'
+require_dependency 'application_controller'
 
 class ShareLayoutsExtension < Radiant::Extension
-  version "0.3"
+  version "0.3.1"
   description "Allows Radiant layouts to be used as layouts for standard Rails actions."
   url "http://wiki.radiantcms.org/Thirdparty_Extensions"
 
