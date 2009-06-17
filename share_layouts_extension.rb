c3c94aa98e337df38f6ac7630ba7659923439cce
mariozig <mariozig@gmail.com>
2009-06-17 09:02:55 +0800

    Change URL to point @ the Github based wiki.  Previous link redirects to a 404.
    Signed-off-by: Sean Cribbs <seancribbs@gmail.com>

--- share_layouts_extension.rb
+++ share_layouts_extension.rb
@@ -4,7 +4,7 @@ require_dependency 'application_controller'
 class ShareLayoutsExtension < Radiant::Extension
   version "0.3.1"
   description "Allows Radiant layouts to be used as layouts for standard Rails actions."
-  url "http://wiki.radiantcms.org/Thirdparty_Extensions"
+  url "http://wiki.github.com/radiant/radiant/thirdparty-extensions"
   
   # I'm sure this can be done more elegantly, but without it, RSpec complains about routing errors
   if ENV["RAILS_ENV"] == "test"
