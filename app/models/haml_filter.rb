e20f6dff8cecf59b71343165fe242432ba2e8f00
Dirk Kelly <dk@dirkkelly.com>
2010-12-02 06:54:31 +0800

    moved filter radius hags to be supported by layotus and snippets

--- app/models/haml_filter.rb
+++ app/models/haml_filter.rb
@@ -1,5 +1,5 @@
 class HamlFilter < TextFilter
   def filter(text)
-    Haml::Engine.new(text).render
+    Haml::Engine.new(text).render.gsub(/&lt;(\/)?r:(.+?)\s*(\/?\\?)&gt;/m,"<\\1r:\\2\\3>")
   end
 end
